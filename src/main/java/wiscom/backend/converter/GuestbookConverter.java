6ab179366ca222c7fea712b24b03903d7d23c01f
hmj6589 <hmj6589@gmail.com>
2025-08-05 00:07:34 +0900

    Feat: 방명록 목록 조회 converter 작성

--- src/main/java/wiscom/backend/converter/GuestbookConverter.java
+++ src/main/java/wiscom/backend/converter/GuestbookConverter.java
@@ -1,8 +1,12 @@
 package wiscom.backend.converter;
 
+import org.springframework.data.domain.Page;
 import wiscom.backend.domain.Guestbook;
 import wiscom.backend.web.dto.GuestbookRequestDTO;
 import wiscom.backend.web.dto.GuestbookResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuestbookConverter {
 
@@ -15,5 +19,28 @@ public class GuestbookConverter {
                 .build();
     }
 
+    public static GuestbookResponseDTO.getGuestbookDTO getGuestbookDTO(Guestbook guestbook) {
+        return GuestbookResponseDTO.getGuestbookDTO.builder()
+                .id(guestbook.getId())
+                .author(guestbook.getAuthor())
+                .recipient(guestbook.getRecipient())
+                .content(guestbook.getContent())
+                .createdAt(guestbook.getCreatedAt())
+                .build();
+    }
 
+    public static GuestbookResponseDTO.getGuestbookListDTO getGuestbookListDTO(Page<Guestbook> guestbooks) {
+        List<GuestbookResponseDTO.getGuestbookDTO> getGuestbookDTOList = guestbooks.stream()
+                .map(GuestbookConverter::getGuestbookDTO)
+                .collect(Collectors.toList());
+
+        return GuestbookResponseDTO.getGuestbookListDTO.builder()
+                .guestbooks(getGuestbookDTOList)
+                .listSize(getGuestbookDTOList.size())
+                .totalPage(guestbooks.getTotalPages())
+                .totalElements(guestbooks.getTotalElements())
+                .isFirst(guestbooks.isFirst())
+                .isLast(guestbooks.isLast())
+                .build();
+    }
 }
