c6e93f61386ea7174e1bf60ce43066210eda48ae
HaSam <ssam8610@naver.com>
2025-08-05 18:13:38 +0900

    Feat: 작품 API 컨트롤러 작성

---
+++ src/main/java/wiscom/backend/web/controller/WorkController.java
@@ -0,0 +1,35 @@
+package wiscom.backend.web.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import wiscom.backend.apiPayload.ApiResponse;
+import wiscom.backend.domain.CategoryEnum;
+import wiscom.backend.service.WorkService;
+import wiscom.backend.service.WorkServiceImpl;
+import wiscom.backend.web.dto.WorkResponseDTO;
+import wiscom.backend.web.dto.WorkDetailResponseDTO;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@Tag(name="Works API", description = "작품 조회 API입니다.")
+public class WorkController {
+
+    private final WorkService workService;
+
+    @GetMapping("/api/workList/{category}")
+    public ApiResponse<List<WorkResponseDTO>>  getWorksListByCategory(@PathVariable("category") String category){
+        List<WorkResponseDTO> works = workService.getWorksByOptionalCategory(category);
+        return ApiResponse.onSuccess(works);
+    }
+
+    @GetMapping("/api/workDetail/{category}/{id}")
+    public ApiResponse<WorkDetailResponseDTO> getWorkDetail(@PathVariable("category") String category, @PathVariable("id") Long id){
+        WorkDetailResponseDTO work = workService.getWorkDetail(category, id);
+        return ApiResponse.onSuccess(work);
+    }
+}
