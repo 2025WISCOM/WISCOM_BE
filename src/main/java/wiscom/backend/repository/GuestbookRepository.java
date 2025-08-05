898c17a67d155a97f6bd6691645facba3e2561bb
hmj6589 <hmj6589@gmail.com>
2025-08-05 15:01:57 +0900

    Feat: 방명록 검색 repository 작성

--- src/main/java/wiscom/backend/repository/GuestbookRepository.java
+++ src/main/java/wiscom/backend/repository/GuestbookRepository.java
@@ -1,7 +1,16 @@
 package wiscom.backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import wiscom.backend.domain.Guestbook;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+
+    @Query("SELECT g FROM Guestbook g WHERE " +
+            "g.author LIKE %:keyword% OR " +
+            "g.recipient LIKE %:keyword% OR " +
+            "g.content LIKE %:keyword%")
+    Page<Guestbook> searchGuestbookByKeyword(String keyword, Pageable pageable);
 }
